8516eb54e3b4e606ff721a5576025ffbb3188b74
Nathanim1919 <nathanim2tadele@gmail.com>
2023-11-10 22:55:20 +0300

    change box bgColor

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -23,18 +23,26 @@ useEffect(() => {
   return (
     <ServiceStyle id='service'>
     <div className='header' data-aos="fade-up">
-      <h3>Services</h3>
-      <h1>What I Am Great At</h1>
-      <p>I excel in building responsive websites, mobile apps, and implementing chatbots and AI automations for enhanced user experiences.</p>
-    </div>  
+        <h3>Expertise</h3>
+        <h2>My Areas of Specialization</h2>
+        <p>I specialize in crafting engaging and visually appealing user interfaces for websites, ensuring a seamless and interactive experience for your audience.</p>
+    </div>
+
 
      <div className='services'>
         <div data-aos = "fade-up" >
             <div>
                 <SiSimilarweb/>
             </div>
-            <h2>Web Development</h2>
-            <p>I specialize in creating responsive and user-friendly websites that cater to your specific needs and objectives.</p>
+            <h2>Front-end Development</h2>
+            <p>I specialize in crafting engaging and visually appealing user interfaces for websites, ensuring a seamless and interactive experience for your audience.</p>
+        </div>
+        <div data-aos = "fade-up" >
+            <div>
+                <AiFillRobot/>
+            </div>
+            <h2>Back-end Development</h2>
+             <p>With a focus on server-side logic and database management, I excel in building robust and scalable back-end systems to power your web applications.</p>
         </div>
         <div data-aos = "fade-up" >
             <div>
@@ -42,13 +50,6 @@ useEffect(() => {
             </div>
             <h2>App Development</h2>
             <p>I have extensive experience in designing and developing mobile applications that deliver seamless experiences on various platforms.</p>
-        </div>
-        <div data-aos = "fade-up" >
-            <div>
-                <AiFillRobot/>
-            </div>
-            <h2>AI Automation</h2>
-            <p>With a focus on AI and automation, I can optimize your business processes, enhance efficiency, and provide intelligent solutions.</p>
         </div>
     </div>
 
@@ -85,8 +86,8 @@ const ServiceStyle = styled.div`
             color: orange;
         }
 
-        h1{
-            font-size: 2rem;
+        h2{
+            font-size: 1.3rem;
         }
 
         p{
@@ -106,9 +107,45 @@ const ServiceStyle = styled.div`
         padding: 1rem;
         box-shadow: 0 9px 20px rgba(0,0,0,.061);
         gap: .6rem;
+        border-radius: 10px;
+        border-left: 10px solid #333;
+        background-color:#fff;
+        position: relative;
+        overflow: hidden;
+        border:1px solid #333;
+        cursor: pointer;
+
+        >*{
+            position: relative;
+            z-index: 5;
+        }
+
+         &:hover
+         {
+            color:#fff;
+         }
+
+        &:hover::after{
+            width: 100%;height: 100%;
+            border-radius: 0;
+        }
+
+        &::after{
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 24%;
+            height: 34%;
+            background-color: #333;
+            content: '';
+            border-bottom-right-radius: 90%;
+            transition: all .3s ease-in-out;
+            transition-timing-function: cubic-bezier(0.455, 0.03, 0.515, 0.955);
+        }
 
         >div{
             font-size: 2rem;
+            color: #fff;
 
         }
 
